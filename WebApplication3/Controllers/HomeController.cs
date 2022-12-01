90cd757968c9abdfb73b604ba9c172c9e4784c6a
abtin <abtinravesh@gmail.com>
2022-12-01 07:46:12 -0600

    2

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Games()
+        {
+            VideoGameRepository v = new VideoGameRepository();
+            return View(v);
+        }
+
+        public IActionResult GameDetails(string g)
+        {
+            VideoGameRepository v = new VideoGameRepository();
+            VideoGame ga = null;
+            foreach(var gam in v.VideoGames)
+            {
+                if(gam.Title == g)
+                {
+                    ga = gam;
+                }
+            }
+            return View(ga);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication3/Models/Company.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication3.Models;
+
+namespace WebApplication3.Models
+{
+    abstract public class Company
+    {
+        public string Name { get; }
+        public double Income { get; private set; }
+
+        public Company(string name, double income)
+        {
+            Name = name;
+            Income = income;
+        }
+    }
+
+    public class Studio : Company
+    {
+        private HashSet<VideoGame> _videoGames { get; set; } = new();
+        public HashSet<VideoGame> VideoGames { get { return _videoGames.ToHashSet(); } }
+
+        public Studio(string name, double income) : base(name, income) { }
+
+        public void AddGame(VideoGame game)
+        {
+            _videoGames.Add(game);
+        }
+    }
+
+    public class Publisher : Company
+    {
+        private HashSet<Studio> _studios { get; set; } = new();
+        public HashSet<Studio> Studios { get { return _studios.ToHashSet(); } }
+
+        public Publisher(string name, double income) : base(name, income) { }
+
+        public void AddStudio(Studio studio)
+        {
+            _studios.Add(studio);
+        }
+    }
+}
